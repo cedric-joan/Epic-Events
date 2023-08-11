3abdc68b25c6413bc669351efba53ef828c6ea26
cecilien <cecilien.joan@gmail.com>
2023-08-11 21:12:06 +0200

    add tables contract, event

--- events_app/models.py
+++ events_app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy import create_engine, Column, Integer, String, Date, Float, ForeignKey, Boolean, Text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine("postgresql+psycopg2://joan:admin@localhost:5432/EPIC", echo=True)
@@ -19,9 +19,43 @@ class Client(Base):
     compagny_name = Column(String(25))
     date_created = Column(Date)
     date_updated = Column(Date)
-    sales_contact = Column(Integer)
+    sales_contact = Column(ForeignKey("usermanager.id"))
 
     def __repr__(self):
         return f"{self.first_name} {self.email} {self.phone_number}"
 
+
+class Contract(Base):
+    __tablename__ = "contract"
+
+    id = Column(Integer, primary_key=True)
+    client_id = Column(ForeignKey("client.id"))
+    sales_contact_id = Column(Integer)
+    total_amount = Column(Float)
+    remaining_amount = Column(Float)
+    date_created = Column(Date)
+    contract_status = Column(Boolean)
+
+    def __repr__(self):
+        return f"{self.client_id} {self.date_created} {self.contract_status}"
+
+
+class Event(Base):
+    __tablename__ = "event"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(25))
+    contract_id = Column(ForeignKey("contract.id"))
+    client_name = Column(String(20))
+    client_contact = Column(String(20))
+    event_date_start = Column(Date)
+    event_date_end = Column(Date)
+    support_contact = Column(String(25))
+    location = Column(String(25))
+    attendees = Column(Integer)
+    notes = Column(String(300))
+
+    def __repr__(self):
+        return f"{self.title} {self.event_date_end} {self.location}"
+    
 Base.metadata.create_all(engine)
